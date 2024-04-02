8ccb0fc0c6bffdfab8c38dc14a0abdf578c106d8
carlosrdk <carloslima36_@outlook.com>
2024-04-02 19:47:50 -0300

    Implementando classes e testes

--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -1,3 +1,73 @@
-public class CalculadoraTest {
-    
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import br.edu.iftm.tspi.Calculadora;
+
+class CalculadoraTest {
+
+    private static Calculadora calc;
+
+    @BeforeAll
+    static void inicializarTeste() {
+        System.out.println("Neste iniciado!");
+    }
+
+    @BeforeEach
+    void instanciarObjetos() {
+        calc = new Calculadora(5);
+    }
+
+    @Test
+    void somar() {
+        System.out.println("Teste de soma!");
+
+        int valor = -5;
+        int resultadoEsperando = 0;
+        calc.somar(valor);
+
+        int resultadoObtido = calc.getMemoria();
+
+        Assertions.assertEquals(resultadoEsperando, resultadoObtido);
+    }
+
+    @Test
+    void subtrair() {
+        int valor = 10;
+        int resultadoEsperando = -5;
+        calc.subtrair(valor);
+
+        int resultadoObtido = calc.getMemoria();
+
+        Assertions.assertEquals(resultadoEsperando, resultadoObtido);
+    }
+
+    @Test
+    void multiplicar() {
+    }
+
+    @Test
+    void dividir() {
+    }
+
+    @Test
+    void exponenciar() {
+    }
+
+    @Test
+    void zerarMemoria() {
+    }
+
+    @AfterEach
+    void finalizarCadaMetodoTeste() {
+        System.out.println("Finalizando caso de teste");
+    }
+
+    @AfterAll
+    static void finalizarTeste() {
+        System.out.println("Fim do teste!!!");
+    }
 }
